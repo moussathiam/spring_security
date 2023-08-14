5713d5918514b358fc0554fbfe578ed001ea21cb
moussa thiam <moussathiam80@gmail.com>
2023-08-14 17:25:49 +0000

    first commit

---
+++ src/test/java/tp/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
